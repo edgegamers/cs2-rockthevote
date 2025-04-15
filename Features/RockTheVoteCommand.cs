a2fc6982a2ca955e08b853ca8576abfc01ad58f9
Kaleb Schmenk <kschmenk@outlook.com>
2025-04-15 02:08:44 -0400

    Undo perm bypass

--- Features/RockTheVoteCommand.cs
+++ Features/RockTheVoteCommand.cs
@@ -20,9 +20,8 @@ public partial class Plugin {
     return HookResult.Continue;
   }
 
-  [ConsoleCommand("forcertvbypass", "Force an RTV to happen immediately regardless of the RTV votes")]
-  //[ConsoleCommand("forcertv", "Force an RTV to happen immediately regardless of the RTV votes")]
-  //[RequiresPermissions("@css/changemap")]
+  [ConsoleCommand("forcertv", "Force an RTV to happen immediately regardless of the RTV votes")]
+  [RequiresPermissions("@css/changemap")]
   public void OnForceRTV(CCSPlayerController? player, CommandInfo? command){
     if (player == null) return;
 
